733513c542e2e5cfe9eb91df72ab9cd0369bdb3a
D-Tr3x <owosenidaniel13@mail.com>
2024-06-24 21:21:42 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main(void) - serves as the entry point for the program
- * printf() - prints the exact string asigned to it and uses a newline character
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
