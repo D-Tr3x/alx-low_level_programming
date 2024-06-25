18d89228e035f55af2714e1d9649aaed49e38f9c
D-Tr3x <owosenidaniel13@mail.com>
2024-06-24 21:18:06 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main(void) - Entry point
- * puts() - Prints the exact string with a newline character
+ * main - Entry point
  *
- * return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
