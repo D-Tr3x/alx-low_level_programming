84eae9fb47e27e5b86dd082ef82e96c629ebd487
D-Tr3x <owosenidaniel13@mail.com>
2024-06-26 20:31:13 -0700

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _islower(int c);
 int print_last_digit(int n);
 int print_sign(int n);
 int _putchar(char c);
+void jack_bauer(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 
