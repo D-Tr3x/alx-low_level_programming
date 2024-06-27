d6af8c9cfaca3677552342403e7851a997b23267
D-Tr3x <owosenidaniel13@mail.com>
2024-06-26 18:29:24 -0700

    Write a function that prints the last digit of a number.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _abs(int n);
-int _islower(int c);
 int _isalpha(int c);
+int _islower(int c);
+int print_last_digit(int n);
 int print_sign(int n);
 int _putchar(char c);
 void print_alphabet(void);
