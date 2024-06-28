895d8918b072cde20abdd13feb82f5909970d50e
D-Tr3x <owosenidaniel13@mail.com>
2024-06-27 17:30:32 -0700

    Write a function that checks for uppercase character.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * positive_or_negative - 
+ *
+ *
+ */
+
+void test_positive_or_negative(int i)
+{
+  if (i < 0)
+    printf("%d is negative", i);
+  else if (i > 0)
+    printf("%d is positive", i);
+  else
+    printf("%d is zero", i);
+}
