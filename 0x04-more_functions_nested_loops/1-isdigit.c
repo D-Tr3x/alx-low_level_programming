4d2427764fb521e58dfd0beb79a05e2dae2cc1e8
D-Tr3x <owosenidaniel13@mail.com>
2024-06-27 18:07:32 -0700

    Checking conformity to Betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks through a character for a digit
+ * _isdigit - checks if a character is a digit (0 through 9)
  * @c: the character to be checked
  *
  * Return: 1 if c is a digit, 0 otherwise
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-if (c >= '0' && c < '9')
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);
