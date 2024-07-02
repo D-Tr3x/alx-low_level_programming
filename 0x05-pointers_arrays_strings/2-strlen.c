88657ae6f66a18cc2e4366d43a90fe3262fdc9d5
D-Tr3x <owosenidaniel13@mail.com>
2024-07-01 21:18:28 -0700

    Write a function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - calculates the length of a string
  * @s: points to th string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
 int len = 0;
 while (s[len] != '\0')
 len++;
-return len;
+return (len);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 int _strlen(char *s);
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _putchar(char);
 
 #endif /* MAIN_H */
