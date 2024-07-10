739f7e0774ef1592f3dadc430387e6c2e42a4482
D-Tr3x <owosenidaniel13@mail.com>
2024-07-10 14:38:16 -0700

    Write a function that prints a string in reverse.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,8 +3,6 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string to print
- *
- * Return: s
  */
 void _puts_recursion(char *s)
 {
@@ -13,7 +11,6 @@ if (*s == '\0')
 _putchar('\n');
 return;
 }
-
 _putchar(*s);
 _puts_recursion(s + 1);
 }
