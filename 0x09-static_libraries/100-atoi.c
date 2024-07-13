dac29754bc89a0c55b75a8f60bde20ec2c5a2062
D-Tr3x <owosenidaniel13@mail.com>
2024-07-12 18:37:55 -0700

    Create the static library libmy.a containing all the functions listed

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * atoi - changes string to integer
+ * @s: the string
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+while (*s)
+s++;
+return (0);
+}
