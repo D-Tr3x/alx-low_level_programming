d8f873a7aeafe25557f567c77a1dddc6567214e8
D-Tr3x <owosenidaniel13@mail.com>
2024-07-19 17:32:29 -0700

    Write a function that creates an array of integers, containing all the values from min (included) to max (included), ordered from min to max.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array containing
+ * all values from min to max
+ * @min: the minimum value of the array
+ * @max: the maximum value of the array
+ *
+ * Return: pointer to the newly cretated array,
+ * NULL on error.
+ */
+int *array_range(int min, int max)
+{
+int *array;
+int length, i;
+length = max - min + 1;
+
+if (min > max)
+return (NULL);
+
+array = malloc(length *sizeof(array));
+if (array == NULL)
+return (NULL);
+
+for (i = min; i < length; i++)
+{
+array[i] = min + i;
+}
+
+return (array);
+}
