987d03d31bcdbefb115ee3281273b890ce29c948
D-Tr3x <owosenidaniel13@mail.com>
2024-07-23 15:49:50 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,55 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * dog_t *new_dog - 
- * @name:
- * @age:
- * @owner:
+ * *new_dog - creates a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  *
- * Return: NULL if function fails
+ * Return: NULL if function fails,
+ * otherwise a pointer to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+	char *new_name, *new_owner;
+	int i, j;
+
+	if (name ==  NULL || owner == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	/* Memory allocation for new dog struct */
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	/*Memory allocation for new_name*/
+	new_name = malloc(i + 1);
+	if (new_name == NULL)
+	{
+	free(new_dog);
+	return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+	/* Memory allocation for new_owner */
+	new_owner = malloc(j + 1);
+	if (new_owner == NULL)
+	{
+	free(new_name);
+	free(new_dog);
+	return (NULL);
+	}
+	for (j = 0; owner[j] != '\0'; j++)
+		new_owner[j] = owner[j];
+	new_owner[j] = '\0';
+	/* Setting new dog's attributes */
+	new_dog->name = new_name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = new_owner;
+	return (new_dog);
 }
