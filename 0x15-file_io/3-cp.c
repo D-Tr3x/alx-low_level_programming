2eefd54b231a7f841c3c3dc91cd64b7813eeb700
D-Tr3x <owosenidaniel13@mail.com>
2024-08-18 07:56:05 -0700

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		_perror("Error: Can't read fro file", argv[1], 98);
+		_perror("Error: Can't read from file", argv[1], 98);
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
