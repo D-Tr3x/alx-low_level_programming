2da94c4b9c64a6f259c1958b19a12663b667c796
D-Tr3x <owosenidaniel13@gmail.com>
2024-11-11 20:38:04 -0800

    Betty coding style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *table;
-  unsigned long int i;
+hash_table_t *table;
+unsigned long int i;
 
-  table = malloc(sizeof(hash_table_t));
-  if (!table)
-    return (NULL);
+table = malloc(sizeof(hash_table_t));
+if (!table)
+return (NULL);
 
-  table->size = size;
-  table->array = malloc(size * sizeof(hash_node_t *));
-  if (!table->array)
-  {
-    free(table);
-    return (NULL);
-  }
+table->size = size;
+table->array = malloc(size * sizeof(hash_node_t *));
+if (!table->array)
+{
+free(table);
+return (NULL);
+}
 
-  for (i = 0; i < table->size; i++)
-    table->array[i] = NULL;
+for (i = 0; i < table->size; i++)
+table->array[i] = NULL;
 
-  return (table);
+return (table);
 }
