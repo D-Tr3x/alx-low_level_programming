120020de97dd31882e449ffb8f5fed75312df4a1
D-Tr3x <owosenidaniel13@gmail.com>
2024-11-13 13:49:26 +0100

    Write a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: the hash table where key/value pair is updated or added
+ * @key: the key(string) to update or add in the hash table
+ * @value: the value associated with the key
+ *
+ * Return: 1 on success, otherwise 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+char *dup_value;
+unsigned long int index;
+hash_node_t *node, *new_node;
+
+if (!ht || !key || key[0] == '\0' || !value)
+return (0);
+
+dup_value = strdup(value);
+if (!dup_value)
+return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+
+node = ht->array[index];
+while (node)
+{
+if (strcmp(node->key, key) == 0)
+{
+free(node);
+node->value = dup_value;
+return (1);
+}
+node = node->next;
+}
+
+new_node = malloc(sizeof(hash_node_t));
+if (!new_node)
+{
+free(dup_value);
+return (0);
+}
+
+new_node->key = strdup(key);
+new_node->value = dup_value;
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+
+return (1);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,3 +1,4 @@
 0. >>> ht = {}
 1. djb2
 2. key -> index
+3. >>> ht['betty'] = 'cool'
