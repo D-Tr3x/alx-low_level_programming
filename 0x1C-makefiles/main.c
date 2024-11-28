2b18fcec3bb14ba1edce1eaea27d90f91f699b75
D-Tr3x <owosenidaniel13@gmail.com>
2024-11-28 03:39:36 +0100

    Create your first Makefile, rules: all

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "school.h"
+
+/**
+ * main - Entry
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+    print_school();
+    return (0);
+}
---
+++ 0x1C-makefiles/school.h
@@ -0,0 +1,8 @@
+#ifndef __SCHOOL_H__
+#define __SCHOOL_H__
+
+#include <stdio.h>
+
+void print_school(void);
+
+#endif
